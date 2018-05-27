63de15e23d754be28eb3a3b934818eb20cf8a787
Watanuki Wolf <watanuki.wolf.2018@gmail.com>
2018-05-27 14:16:31 +0900

    trivial modify

--- hello.pl
+++ hello.pl
@@ -7,10 +7,10 @@ f(Acc, N, X):-
     f(A, N_1, X).
 
 f(Acc, N, X):-
-        N >= 0,
-        append(Acc, [1], A),
-        N_1 is N - 1,
-        f(A, N_1, X).
+    N >= 0,
+    append(Acc, [1], A),
+    N_1 is N - 1,
+    f(A, N_1, X).
 
 number_of_combination_consist_of_0_1(N, X):-
     f([], N, X).
